4e289a3e77e43e543429d20f77ead8bb8ddc770d
ArunMKumar <arunkr.linux@gmail.com>
2014-06-01 01:57:09 +0000

    added Multiple teams support

--- score-card/src/Scores.py
+++ score-card/src/Scores.py
@@ -1,5 +1,7 @@
 # Python script to display the latest scores
-# Arun Kumar, Varun Gupta 
+# Arun Kumar	@ArunMKumar
+# Varun Gupta	varun90gupta@gmail.com
+# Anish Tambe 	@atdaemon
 # 01.06.2014
 
 
@@ -13,10 +15,10 @@ import re
 
 ####################### Constatnts #############################
 
-teams = ["England", "India", "Sri Lanka", "West Indies", "Punjab T", "Chennai T"]
+teams = ["England", "India", "Sri Lanka", "West Indies", "Punjab T", "Chennai T", "Yorkshire", "Hampshire", "Surrey", "Sussex" ]
 rows = {"time" : 0 , "team1" : 1 , "team2" : 2 , "player" : 3}
-
-
+colm = {"teamname": 0 ,"score": 12, "time": 11}
+namelen = 7
 ###################### functions ##############################
 
 display = None
@@ -40,7 +42,6 @@ def getFavsIds(matchesList,favTeams) :
 	for match in matchesList :
 		if match['t1'] in favTeams or match['t2'] in favTeams :
 			favs.append(match['id'])
-	print(favs)
 	return favs
 
 def getMatchScore(matchId):
@@ -50,11 +51,18 @@ def getMatchScore(matchId):
 	return eval(Response.read())  ## Disputed
 
 def getTeamNameandScores(status):
-	#print(status)
-	#Result = re.search("([a-zA-Z\ ]+)([0-9]+\/[0-9]+)[\ \*]*v([a-zA-Z\ ]+)([0-9]+\/[0-9]+)",status)
-	#names = [Result.group(1), Result.group(2), Result.group(3), Result.group(4)]
-	names = status.split(" v ");
+	Result = re.search("([a-zA-Z\ ]+).*([0-9]+\/[0-9]+)*[\ \*]*v([a-zA-Z\ ]+).*([0-9]+\/[0-9]+)*",status)
+	names = [Result.group(1), Result.group(2), Result.group(3), Result.group(4)]
 	return names
+
+def displaytime():
+	global display
+	display.cursor_pos = (0,0)
+	Current_Time =time.strftime("%d %b %y")
+	display.write_string(Current_Time)
+	display.cursor_pos = (rows['time'], colm['time'])
+	Current_Time = time.strftime(" %H:%M:%S")
+	display.write_string(Current_Time)
 
 
 ###############################################################################
@@ -65,74 +73,38 @@ initLCD()
 #Get list of matches
 matches = getMatchList()
 
-print(matches)
+
 #Get the Match ID
 matchIds = getFavsIds(matches, teams)
 
-print(matchIds)
 
 #get the status for all matches
 statuses = getMatchScore(matchIds)  # all scores
-
+print(statuses)
 # Display Scores
 
 for status in statuses:
 	
-	s = getTeamNameandScores(status['si'])
-	#display
-	#display.clear()
+	nameAndScore = getTeamNameandScores(status['si'])
+
+	display.clear()
+	displaytime()
+
+	if nameAndScore[0]:
+		display.cursor_pos = (rows["team1"],colm['teamname'])
+		display.write_string(nameAndScore[0].strip()[:namelen])
+
+	if nameAndScore[1]:
+                display.cursor_pos = (rows["team1"],colm['score'])
+                display.write_string(nameAndScore[1].strip())
+
+ 	if nameAndScore[2]:
+                display.cursor_pos = (rows["team2"],colm['teamname'])
+                display.write_string(nameAndScore[2].strip()[:namelen])
+
+
+ 	if nameAndScore[3]:
+                display.cursor_pos = (rows["team2"],colm['score'])
+                display.write_string(nameAndScore[3].strip())
 	
-	display.cursor_pos = (rows["team1"],0)
-	display.write_string(s[0])
-	#display.cursor_pos = (rows["team1"], 14)
-	#display.write_string(s[1])
-
-	display.cursor_pos = (rows["team2"],0)
-	display.write_string(s[1])
-	#display.cursor_pos = (rows["team2"], 14)
-	#display.write_string(s[3])
-
-	#time.sleep(60/ len(statuses))
-	print("hello")
-
-
-display.cursor_pos = (rows["time"],0)
-Current_Time =time.strftime("%d %b %y")
-display.write_string(Current_Time)
-
-
-display.cursor_pos = (rows["time"],11)
-Current_Time = time.strftime(" %H:%M:%S")
-display.write_string(Current_Time)
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-#favs = getFavsIds(matches,teams)
-
-
-#score = getMatchScore(str(favs[0]))
-#l = eval(score)
-#text = l[0]['si']
-#index = text.find(" v ")
-
-
-#display.cursor_pos = (1,0)
-#display.write_string(text[:index])
-#display.cursor_pos = (2,0)
-#display.write_string(text[index+3:])
-#print(text)
-
-#display.write_string(text)
-
+	time.sleep(60/len(matchIds))		# sleep accordingly
